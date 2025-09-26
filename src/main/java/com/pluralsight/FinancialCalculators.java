69dac2fdedd238625b1f56ad0b2c48b4d50b8b90
Avi <95044844+Avioti@users.noreply.github.com>
2025-09-26 13:33:05 -0400

    Finished Financial Calculators

--- src/main/java/com/pluralsight/FinancialCalculators.java
+++ src/main/java/com/pluralsight/FinancialCalculators.java
@@ -5,34 +5,52 @@ import java.util.Scanner;
 
 public class FinancialCalculators {
     public static void main(String[] args) {
+
 calcChoice();
+
     }
-public static void calcChoice(){
+
+
+    public static void calcChoice(){
         Scanner scanner = new Scanner(System.in);
+
     System.out.println("Which calculator will you like today?");
-    System.out.println("Mortage(M/1), FutureValue(Fv/2)");
+
+    System.out.println("Mortage(M/1), FutureValue(Fv/2), PresentValue(Pv/3)");
+
     String calcChoice = scanner.next();
+
     if (calcChoice.equalsIgnoreCase("Mortgage") || calcChoice.equalsIgnoreCase("M") || calcChoice.equalsIgnoreCase("1")){
         mortgageCalc();
-    } else if (calcChoice.equalsIgnoreCase("FutureValue") || calcChoice.equalsIgnoreCase("Fv") || calcChoice.equalsIgnoreCase("2")){
+    }
+    else if (calcChoice.equalsIgnoreCase("FutureValue") || calcChoice.equalsIgnoreCase("Fv") || calcChoice.equalsIgnoreCase("2")){
         futureValueCalc();
     }
+    else if (calcChoice.equalsIgnoreCase("PresentValue") || calcChoice.equalsIgnoreCase("Pv") || calcChoice.equalsIgnoreCase("3")){
+        presentValueCalc();
+    }
 }
+
+
     public static void mortgageCalc(){
 
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("What is the Total amount of the loan: ");
         double Principal = scanner.nextDouble();
+
         System.out.println("What is the Interest Rate: ");
         double Interest_rate = scanner.nextDouble();
         if (Interest_rate != 0){
-            Interest_rate = Interest_rate / 100;
+            Interest_rate = interestConversion(Interest_rate);
         }
         double Monthly_Interest = Interest_rate / 12;
+
         System.out.println("Estimated Length of loan? ");
         double Loan_length = scanner.nextDouble();
         System.out.println("Years or Months");
         String Length_amount = scanner.next();
+
         if (Length_amount.equalsIgnoreCase("Years" ) || Length_amount.equalsIgnoreCase("Year") || Length_amount.equalsIgnoreCase("Y" )){
             Loan_length = 12 * Loan_length;
 
@@ -48,13 +66,18 @@ public static void calcChoice(){
 
     }
 
+
+
     public static void futureValueCalc(){
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter deposit amount: ");
         double Initial_deposit = scanner.nextDouble();
+
         System.out.println("What is the interest rate: ");
         double Interest_rate = scanner.nextDouble();
-        double Annual_interest = Interest_rate / 100;
+        double Annual_interest = interestConversion(Interest_rate);
+
         System.out.println("How many years will this deposit gain interest: ");
         double Owned_length = scanner.nextDouble();
 
@@ -63,7 +86,29 @@ public static void calcChoice(){
         System.out.printf("The future value of this cd is %.2f" + " and the Total interest paid is %.2f", Future_value , Total_interest);
     }
 
-    public static void presentValueCalc(){
 
+
+    public static void presentValueCalc(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is the monthly payout?");
+        double Monthly_payout = scanner.nextDouble();
+
+        System.out.println("What is the expected interest rate?");
+        double Expected_rate = scanner.nextDouble();
+        double Interest_rate = interestConversion(Expected_rate) / 12;
+
+        System.out.println("How many years to payout?");
+        double  Years = scanner.nextDouble() * 12;
+
+        double Present_value = Monthly_payout * (1 - Math.pow(1 + Interest_rate, -Years)) / Interest_rate;
+
+        System.out.printf("To fund an annuity that pays $%.2f monthly for %.2f years and earns an expected " + Expected_rate + "%% interest, you would need to invest %.2f today", Monthly_payout, Years, Present_value);
+    }
+
+
+
+    public static double interestConversion(double x){
+        return x / 100;
     }
 }
